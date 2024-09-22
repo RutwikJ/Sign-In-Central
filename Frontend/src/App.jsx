40e017b060cca0a0f7fa60e32a3bdfece8568a8a
Rutwik <rutwik1996@gmail.com>
2024-09-22 18:09:52 +0530

    created pages using react router dom and created header component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import SignUp from './pages/SignUp'
+import SignIn from './pages/SignIn'
+import Header from './Components/Header'
+
 
 function App() {
   return (
-    <div className='text-green-500'>App</div>
-  )
+ <BrowserRouter>
+    <Header/>
+        <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/profile' element={<Profile/>}/>
+              <Route path='/sign-in' element={<SignIn/>}/>
+              <Route path='/sign-up' element={<SignUp/>}/>
+              <Route path='/about' element={<About/>}/>
+        </Routes>
+ </BrowserRouter>
+  ) 
 }
 
 export default App
