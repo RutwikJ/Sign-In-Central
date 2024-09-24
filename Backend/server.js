40178e30c0eb883f6e854c5a9601628e7fe717f4
Rutwik <rutwik1996@gmail.com>
2024-09-24 18:52:05 +0530

    connected to DB

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,19 @@
-import express from 'express'
-const app =express()
-const port=5000
-app.listen(port,()=>{
-    console.log(`server is running on port ${port} !!!`);
-    
-})
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+const port = 5000;
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port} !!!`);
+});
